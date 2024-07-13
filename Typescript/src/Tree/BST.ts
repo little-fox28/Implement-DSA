77f0ffef6332c5275f80289adaeb74a84bd9175b
little-fox28 <tranthjnh288@gmail.com>
2024-07-13 13:16:33 +0700

    Done PostOrderTraverse in Tree and tested

--- Typescript/src/Tree/BST.ts
+++ Typescript/src/Tree/BST.ts
@@ -6,6 +6,7 @@ import retryTimes = jest.retryTimes;
 import {StackADT} from "../Stacks/StackADT";
 import {LinkedListBaseStack} from "../Stacks/LinkedListBaseStack";
 import NODE from "../LinkedList/Node";
+import nodeTree from "./NodeTree";
 
 type NodeTreeType = NodeTree | null;
 
@@ -139,8 +140,41 @@ class BinarySearchTree implements TreeADT {
     }
 
     private PostOrderTraverse(): number[] {
-        return [];
+        const result: number[] = [];
+        const expectedCount = this.nodeCount;
+        const stack: StackADT<nodeTree> = new LinkedListBaseStack();
+        const postOrderStack: StackADT<nodeTree> = new LinkedListBaseStack();
 
+        if (this.root === null) {
+            throw new Error("Tree Empty");
+        }
+
+        stack.push(this.root);
+
+        while (!stack.isEmpty()) {
+            if (expectedCount !== this.nodeCount) {
+                throw new Error("Concurrent Modification Exception");
+            }
+
+            const currentNode = stack.pop();
+            postOrderStack.push(currentNode);
+
+            if (currentNode!.getLeft() !== null) {
+                stack.push(currentNode!.getLeft()!);
+            }
+
+            if (currentNode!.getRight() !== null) {
+                stack.push(currentNode?.getRight()!)
+            }
+        }
+
+        while (!postOrderStack.isEmpty()) {
+            const nextNode = postOrderStack.pop();
+            result.push(nextNode!.getData()!)
+        }
+
+
+        return result;
     }
 
 

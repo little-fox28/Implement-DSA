1b092b74ceea6d801f0f0cb4d98e8aa834f0a550
little-fox28 <tranthjnh288@gmail.com>
2024-08-03 10:04:16 +0700

    add feature in Typescript/HashTable/NODE

--- Typescript/src/HashTable/Node.ts
+++ Typescript/src/HashTable/Node.ts
@@ -1,10 +1,10 @@
-class NODE<K,V>{
+class NODE<K, V> {
     private hash: number;
     private key: K;
     private value: V;
 
-    constructor(hash: number, key: K, value: V) {
-        this.hash = hash;
+    constructor(key: K, value: V) {
+        this.hash = this.hashCode(key);
         this.key = key;
         this.value = value;
     }
@@ -31,6 +31,27 @@ class NODE<K,V>{
 
     public setValue(value: V): void {
         this.value = value;
+    }
+
+    public toString(): string {
+        return `Hash:${this.hash} | Key: ${this.key} | Values: ${this.value}`;
+    }
+
+
+    // PRIVATE
+    private hashCode(key: K): number {
+        if (typeof key === 'number') {
+            return key
+        } else if (typeof key === 'string') {
+            let hash = 0;
+            for (let i = 0; i < key.length; i++) {
+                hash = (hash << 5) - hash + key.charCodeAt(i);
+                hash |= 0;
+            }
+            return hash;
+        } else {
+            throw new Error("Unsupported type for HashFunction");
+        }
     }
 }
 
